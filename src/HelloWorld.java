c6affb00dd572ee1f22becf5abc0ef557dd13434
Sriharsha <sriharshavr.y@gmail.com>
2014-03-22 17:02:46 +0530

    Added number operations with base logic

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld
     public static void main(String[] args)
     {
         NumberTriangle triangle = new NumberTriangle(6);
-        //triangle.printTriangle();
+        triangle.printTriangle();
         NumberPyramid.printPyramid(12);
     }
 }
---
+++ src/NumberOperations.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Sriharsha on 22-03-2014.
+ */
+public class NumberOperations {
+    public static void perform(char operation){
+        switch (operation){
+            case '+':
+                break;
+            case '-':
+                break;
+            case '*':
+                break;
+            default:
+                System.out.println("Operation not supported");
+        }
+
+    }
+}
