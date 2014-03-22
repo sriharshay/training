0144a8e0086a23aeaedff24fef0c62b142869b0f
Sriharsha <sriharshavr.y@gmail.com>
2014-03-22 17:07:08 +0530

    Added number operations with updated logic

--- src/NumberOperations.java
+++ src/NumberOperations.java
@@ -1,18 +1,40 @@
 /**
  * Created by Sriharsha on 22-03-2014.
  */
-public class NumberOperations {
-    public static void perform(char operation){
-        switch (operation){
+public class NumberOperations
+{
+    public static void perform(char operation, int operandX, int operandY)
+    {
+        switch (operation)
+        {
             case '+':
+                performAddition(operandX, operandY);
                 break;
             case '-':
+                performSubtraction(operandX, operandY);
                 break;
             case '*':
+                performMultiplication(operandX, operandY);
                 break;
             default:
                 System.out.println("Operation not supported");
         }
 
+    }
+
+    private static void performMultiplication(int operandX, int operandY)
+    {
+        System.out.println(operandX + " * " + operandY + " = " + (operandX * operandY));
+    }
+
+    private static void performSubtraction(int operandX, int operandY)
+    {
+        System.out.println(operandX + " - " + operandY + " = " + (operandX - operandY));
+
+    }
+
+    private static void performAddition(int operandX, int operandY)
+    {
+        System.out.println(operandX + " + " + operandY + " = " + operandX + operandY);
     }
 }
